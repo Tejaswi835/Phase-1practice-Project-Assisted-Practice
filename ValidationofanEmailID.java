78007e6f120d64c78bab6bc0b6986f0b8f3418b5
Tejaswini <97823635+Tejaswi835@users.noreply.github.com>
2023-05-09 00:03:22 +0530

    Add files via upload

---
+++ ValidationofanEmailID.java
@@ -0,0 +1,23 @@
+package ValidationofanEmailID;
+import java.util.*;
+public class ValidationofanEmailID {
+public static void main(String[] args) {
+	        String[] emailIDs = { "tippu@gmail.com", "mickey@gmail.com", "bobby@gmail.com" };
+	        Scanner sc= new Scanner(System.in);
+	        System.out.print("Enter an email ID to search: ");
+	        String searchEmail = sc.nextLine();
+	        boolean match = false;
+	        for (String email : emailIDs) {
+	            if (email.equalsIgnoreCase(searchEmail)) {
+	               match = true;
+	                break;
+	            }
+	        }
+	        if (match) {
+	            System.out.println("Email ID found in the array");
+	        } else {
+	            System.out.println("Email ID not found in the array");
+	        }
+	    }
+}
+
